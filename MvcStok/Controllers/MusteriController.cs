aa92fecb76ab7cc2bf10ace8447e6b0a7dab0ff9
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-03 18:00:01 +0300

    musteriler ve ürünler listelendi

---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var musteri = db.Musteriler.ToList();
+            return View(musteri);
+        }
+    }
+}
---
+++ MvcStok/Controllers/UrunController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        MvcDbStokEntities db=new MvcDbStokEntities();
+        
+        public ActionResult Index()
+        {
+            var degerler=db.Urunler.ToList();
+            return View(degerler);
+        }
+    }
+}
